f90dd1c51eb34f18579547a8b1fcac39605de774
Philipp Krause <38114276+Snaakn@users.noreply.github.com>
2023-03-18 16:40:06 +0100

    Added token action hud css fixes  (#1)
    css improvements for token action hud

--- scripts/character-sheet.js
+++ scripts/character-sheet.js
@@ -1,18 +1,5 @@
-import { MODULE_NAME } from "./consts.js";
+import { injectCSS } from "./utils.js";
 
 Hooks.once("init", async () => {
   injectCSS("character-sheet");
 });
-
-function injectCSS(filename) {
-  const head = document.getElementsByTagName("head")[0];
-  const mainCss = document.createElement("link");
-  mainCss.setAttribute("rel", "stylesheet");
-  mainCss.setAttribute("type", "text/css");
-  mainCss.setAttribute(
-    "href",
-    "modules/" + MODULE_NAME + "/styles/" + filename + ".css"
-  );
-  mainCss.setAttribute("media", "all");
-  head.insertBefore(mainCss, head.lastChild);
-}
