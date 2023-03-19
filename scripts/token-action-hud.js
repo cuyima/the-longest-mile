2e772cc0d755159f43c7a8b048d27e9254d60cb0
cuyima <vivilotta98@live.de>
2023-03-19 23:23:56 +0100

    file renaming, fixed TAH hook

--- scripts/token-action-hud.js
+++ scripts/token-action-hud.js
@@ -1,7 +1,7 @@
 import { MODULE_NAME } from "./consts.js";
 import { injectCSS } from "./utils.js";
 
-Hooks.on("renderTokenActionHud", (app, html, data) => {
-     injectCSS("tlm-tah");
+Hooks.once("renderTokenActionHud", async () => {
+     injectCSS("tah");
      console.log( MODULE_NAME + " | token action hud loaded -> loaded css improvements");
 });
