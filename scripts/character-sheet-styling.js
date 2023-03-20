760140c9b23a31787c2f56f3a01e8969632caf84
cuyima <52758909+cuyima@users.noreply.github.com>
2023-03-20 23:44:47 +0100

    Dervish Update (#3)
    * dervish update for free archetype balancing
    * lots of technical and consistency changes

--- scripts/character-sheet-styling.js
+++ scripts/character-sheet-styling.js
@@ -1,8 +1,7 @@
 import { injectCSS } from "./utils.js";
+import { MODULE_NAME } from "./consts.js";
 
 Hooks.once("init", async () => {
   injectCSS("character-sheet");
-  console.log(
-    MODULE_NAME + " | Injected CSS for character sheets."
-  );
+  console.log(MODULE_NAME + " | Injected CSS for character sheets.");
 });
