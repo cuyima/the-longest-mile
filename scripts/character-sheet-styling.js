175878423fe83629b324f40de88a555159e68908
cuyima <vivilotta98@live.de>
2023-03-20 21:55:18 +0100

    fixed missing import

--- scripts/character-sheet-styling.js
+++ scripts/character-sheet-styling.js
@@ -1,8 +1,6 @@
-import { injectCSS } from "./utils.js";
+import { injectCSS, MODULE_NAME } from "./utils.js";
 
 Hooks.once("init", async () => {
   injectCSS("character-sheet");
-  console.log(
-    MODULE_NAME + " | Injected CSS for character sheets."
-  );
+  console.log(MODULE_NAME + " | Injected CSS for character sheets.");
 });
