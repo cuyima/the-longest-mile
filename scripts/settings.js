e897e92b32915cd6e7a19228a846e801c15e81f4
cuyima <52758909+cuyima@users.noreply.github.com>
2023-05-25 15:48:02 +0200

    Dervish Remastered (#22)

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,21 +1,6 @@
 import { MODULE_NAME } from "./consts.js";
 
 Hooks.once("init", () => {
-    game.settings.register(MODULE_NAME, "dervish-ui", {
-		scope: "user",
-		config: true,
-		name: "Enable Dervish Archetype Automation",
-		hint: "Enable automatic Focus Points consumption and handling of amped spells.",
-		type: Boolean,
-		onChange: () => {
-            const messages = game.messages.filter((m) => m instanceof ChatMessage);
-            for (const message of messages) {
-              ui.chat.updateMessage(message);
-            }
-          },
-		default: true
-	});
-
 	game.settings.register(MODULE_NAME, "sc-hack", {
 		scope: "user",
 		config: true,
