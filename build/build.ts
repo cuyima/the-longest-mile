71aba046282cb78fa42d40dacd58e6f8f87a10b5
cuyima <vivilotta98@live.de>
2023-06-10 22:02:00 +0200

    various testing

--- build/build.ts
+++ build/build.ts
@@ -3,34 +3,55 @@
 
 import fs from "fs-extra";
 import path from "path";
+import { ClassicLevel, type DatabaseOptions } from "classic-level";
+import { isObject } from "remeda";
 
 function myRandomId() {
-    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    return Array.from(Array(16).keys())
-        .map(() => letters[Math.floor(Math.random() * letters.length)])
-        .join("");
+  const letters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  return Array.from(Array(16).keys())
+    .map(() => letters[Math.floor(Math.random() * letters.length)])
+    .join("");
 }
 
 fs.rmSync("temporary", { recursive: true, force: true });
-const outDir = <string>fs.mkdirSync(path.resolve(".", "temporary"), { recursive: true });
+const outDir = <string>(
+  fs.mkdirSync(path.resolve(".", "temporary"), { recursive: true })
+);
 if (!outDir) {
-    throw new Error("Could not create output directory");
+  throw new Error("Could not create output directory");
 }
 
 fs.mkdirsSync(path.resolve(outDir, "packs"));
 
 function getFolders(path: fs.PathLike) {
-    return fs.readdirSync(path).filter(function (file) {
-        return fs.statSync(path + "/" + file).isDirectory();
-    });
+  return fs.readdirSync(path).filter(function (file) {
+    return fs.statSync(path + "/" + file).isDirectory();
+  });
+}
+
+function getItems(path: fs.PathLike) {
+  return fs.readFileSync(path).filter(function (file) {
+    return fs.statSync(path + "/" + file).isFile();
+  });
+}
+
+function getId(item: any) {
+    return isObject(item) && "_id" in item;
 }
 
 function buildPacks() {
-    const folders = getFolders(path.resolve(outDir, "packs/"));
-    folders.forEach(folder => {
-        console.log(folder)
-    });
-    for (const folderPath of folders) {
+  const folders = getFolders("packs/");
+  folders.forEach((folder) => {
+    if (folder.toLowerCase() === "journals") {
+      const db = new ClassicLevel(`dist/${folder}`);
+      getItems(`packs/${folder}`).forEach((item) => {
+        console.log(item);
+        //console.log(getId(item))
+      });
+    }
+  });
+  /*for (const folderPath of folders) {
         const lines: string[] = [];
         const linesInternal: string[] = [];
 
@@ -52,12 +73,12 @@ function buildPacks() {
         //const dir2 = path.resolve(outDir, "packs", "generated", path.basename(folderPath) + "-internal.db");
         //fs.writeFileSync(dir2, linesInternal.join("\n"), "utf8");
     }
-    //fs.rmSync(path.resolve(outDir, "./packs/generated/asymonous-benefactor-macros/"), { recursive: true });
+    //fs.rmSync(path.resolve(outDir, "./packs/generated/asymonous-benefactor-macros/"), { recursive: true });*/
 }
 
 buildPacks();
-fs.rmSync("./dist", { recursive: true, force: true });
+/*fs.rmSync("./dist", { recursive: true, force: true });
 fs.mkdirsSync(path.resolve("dist/packs/"));
 fs.copySync(outDir, "./dist");
 //fs.copySync("./packs/db", "./dist/packs/db");
-fs.rmSync(outDir, { recursive: true, force: true });
+fs.rmSync(outDir, { recursive: true, force: true });*/
