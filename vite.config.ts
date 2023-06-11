18c2bfa128a43ffe5fde13ea404258df30a18ce4
cuyima <vivilotta98@live.de>
2023-06-11 13:47:08 +0200

    added back build features from system

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,66 @@
+//Shamelessly stolen froom pf2e system https://github.com/foundryvtt/pf2e
 import * as Vite from "vite";
+import esbuild from "esbuild";
+import fs from "fs-extra";
+import path from "path"
 
 const config = Vite.defineConfig(({ command, mode }): Vite.UserConfig => {
     const buildMode = mode === "production" ? "production" : "development";
     const outDir = "dist";
+
+    const plugins: any[] = [];
+
+    if (buildMode === "production") {
+        plugins.push({
+            name: "minify",
+            renderChunk: {
+                order: "post",
+                async handler(code: any, chunk: { fileName: string; }) {
+                    return chunk.fileName.endsWith(".mjs")
+                        ? esbuild.transform(code, {
+                              keepNames: true,
+                              minifyIdentifiers: false,
+                              minifySyntax: true,
+                              minifyWhitespace: true,
+                              sourcemap: true,
+                          })
+                        : code;
+                },
+            },
+        });
+    }else{
+        plugins.push(
+            // Foundry expects all esm files listed in system.json to exist: create empty vendor module when in dev mode
+            {
+                name: "touch-vendor-mjs",
+                apply: "build",
+                writeBundle: {
+                    async handler() {
+                        fs.closeSync(fs.openSync(path.resolve(outDir, "vendor.mjs"), "w"));
+                    },
+                },
+            },
+            // Vite HMR is only preconfigured for css files: add handler for HBS templates
+            {
+                name: "hmr-handler",
+                apply: "serve",
+                handleHotUpdate(context: { file: string; server: { ws: { send: (arg0: { type: string; event: string; data: { path: string; }; }) => void; }; }; }) {
+                    if (context.file.endsWith(".hbs") && !context.file.startsWith(outDir)) {
+                        const basePath = context.file.slice(context.file.indexOf("templates/"));
+                        console.log(`Updating template at ${basePath}`);
+                        fs.promises.copyFile(context.file, `${outDir}/${basePath}`).then(() => {
+                            context.server.ws.send({
+                                type: "custom",
+                                event: "template-update",
+                                data: { path: `modules/the-longest-mile/${basePath}` },
+                            });
+                        });
+                    }
+                },
+            }
+        );
+    }
+
     return {
         root: "./",
         base: command === "build" ? "./" : "/modules/the-longest-mile/",
@@ -14,7 +72,7 @@ const config = Vite.defineConfig(({ command, mode }): Vite.UserConfig => {
         build: {
             outDir,
             emptyOutDir: false,
-            minify: false,
+            minify: true,
             sourcemap: true,
             lib: {
                 name: "the-longest-mile",
@@ -46,11 +104,11 @@ const config = Vite.defineConfig(({ command, mode }): Vite.UserConfig => {
                 },
             },
         },
+        plugins,
         css: {
             devSourcemap: true,
         },
     };
 });
 
-// eslint-disable-next-line import/no-default-export
 export default config;
