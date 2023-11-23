0b5b8c7bf1126ec47d5b8c0bceca1cb1b4282854
cuyima <vivilotta98@live.de>
2023-11-23 19:18:35 +0100

    cleanup attempt

--- src/scripts/consts.js
+++ src/scripts/consts.js
@@ -5,8 +5,6 @@ export const TAH = "TokenActionHud";
 export const DERVISH_EFFECT = `Compendium.${MODULE_NAME}.dervish.Item.coWYuzbu9Awoy34r`;
 export const DERVISH_EFFECT_SLUG = `effect-gather-psi`;
 
-export const DERVISH_CHARGE_ACTIONS = ["gather-psi", "ready-mind"];
-
 export const DERVISH_CONSUME_ACTIONS = [
   "psionic-strike",
   "psionic-strike-amped",
@@ -14,5 +12,9 @@ export const DERVISH_CONSUME_ACTIONS = [
   "psionic-movement-spell-amped",
   "guardian-shield-spell",
   "guardian-shield-spell-amped",
-  "psychic-lash"
+  "psychic-lash",
 ];
+
+export const DERVISH_STRIKE_EFFECT = `Compendium.${MODULE_NAME}.dervish.Item.9HtGwdItIbqTfAbn`;
+export const DERVISH_STRIKE_EFFECT_AMP = `Compendium.${MODULE_NAME}.dervish.Item.Of8keBzlkQDYZGRC`;
+export const DERVISH_STRIKE_CHARGE = "psionic-strike";
