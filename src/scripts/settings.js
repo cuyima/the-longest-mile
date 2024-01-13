762db0bc5c5eb48ccc9dd4d715ccfe306a3da47f
vivien-laasch <92919961+vivien-laasch@users.noreply.github.com>
2024-01-14 00:12:57 +0100

    Sheet update (#27)
    character sheet compatibility update
    dorako compatibility update
    sc compatibility update
    extend styling to also cover SC journal sheets

--- src/scripts/settings.js
+++ src/scripts/settings.js
@@ -1,12 +1,33 @@
 import { MODULE_NAME } from "./consts.js";
 
 export function registerSettings() {
-	game.settings.register(MODULE_NAME, "sc-hack", {
-		scope: "user",
-		config: true,
-		name: "Enable Dorako UI Style Calendar",
-		hint: "Enable Simple Calendar styling to mimick Dorako UI.",
-		type: Boolean,
-		default: true
-	  });
+  game.settings.register(MODULE_NAME, "dorako-sc", {
+    scope: "user",
+    config: true,
+    name: "Dorako UI Style Calendar",
+    hint: "Enable to mimick Dorako UI styling on Simple Calendar. This setting will respect sheet theme settings from Dorako UI and automatically disable itself when no Dorako sheet theme is chosen.",
+    type: Boolean,
+    default: true,
+    requiresReload: true,
+  });
+
+  game.settings.register(MODULE_NAME, "dorako-combat-dock", {
+    scope: "user",
+    config: true,
+    name: "Dorako UI Style Carousel Combat Tracker",
+    hint: "Enable to mimick Dorako UI styling on Carousel Combat Tracker. This setting will respect Interface Theme settings from Dorako UI and automatically disable itself when no Dorako interface theme is chosen.",
+    type: Boolean,
+    default: true,
+    requiresReload: true,
+  });
+
+  game.settings.register(MODULE_NAME, "custom-combat-dock", {
+    scope: "user",
+    config: true,
+    name: "Additional styling for Carousel Combat Tracker",
+    hint: "Removes participant borders and backgrounds from the combat dock and restyles the tracked attributes.",
+    type: Boolean,
+    default: true,
+    requiresReload: true,
+  });
 }
